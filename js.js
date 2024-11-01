94b267b47271d5975848ea6f87030d5224508cca
A-Deepak1610 <deepaka.ad24@bitsathy.ac.in>
2024-11-01 14:11:40 +0530

    addd

--- js.js
+++ js.js
@@ -4,7 +4,7 @@ function add(){
     var listitem=document.createElement("li")
     var date=document.getElementById("dtt")
     var datetime=date.value ? date.value :"No date set"
-    listitem.innerHTML="<input type='checkbox' id='cb' onclick='line(event)'>"+input.value+"<span class='due'>Due:</span>"+datetime+"<button class='del' onclick='del(event)'>Delete</button>"
+    listitem.innerHTML="<input type='checkbox' id='cb' onclick='line(event)'>"+input.value+"<span class='due'>Due:</span>"+datetime+'<button class="edit" id="edit" onclick="editt(event)">Edit</button>'+"<button class='del' onclick='del(event)'>Delete</button>"
     if(input.value==""){
         alert("Please Enter a Task!");
     }
@@ -32,4 +32,28 @@ function line(event) {
     } else {
         listItem.style.textDecoration = "none";
     }
+}
+function editt(event){
+    var listItem = event.target.parentElement;
+    console.log(listItem)
+    var taskText = listItem.childNodes[1].textContent;
+    var dueDateText = listItem.querySelector(".due").nextSibling.textContent.trim();
+    listItem.innerHTML = "<input type='checkbox' id='cb' onclick='line(event)'>"
+                        + "<input type='text' id='editTaskText' value='" + taskText + "'>"
+                        + "<span class='due'> Due: </span>"
+                        + "<input type='date' id='editDueDate' value='" + (dueDateText !== "No date set" ? dueDateText : "") + "'>"
+                        + "<button onclick='saveEdit(this)'>Save</button>"
+                        + "<button class='del' onclick='del(event)'>Delete</button>";
+}
+function saveEdit(button) {
+    var listItem = button.parentElement;
+    var newTaskText = document.getElementById('editTaskText').value;
+    var newDueDate = document.getElementById('editDueDate').value || "No date set";
+
+    listItem.innerHTML = "<input type='checkbox' id='cb' onclick='line(event)'>"
+                        + newTaskText
+                        + "<span class='due'> Due: </span>"
+                        + newDueDate
+                        + "<button class='edit' id='edit' onclick='editt(event)'>Edit</button>"
+                        + "<button class='del' onclick='del(event)'>Delete</button>";
 }
